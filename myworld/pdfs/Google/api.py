51b76f0a56f5aafbd27578c061c5af49f1c52660
Sai Kiran Reddy Appidi <98457635+saikiranreddyappidi@users.noreply.github.com>
2023-01-08 23:37:15 +0530

    Update api.py

--- myworld/pdfs/Google/api.py
+++ myworld/pdfs/Google/api.py
@@ -1,6 +1,6 @@
 import json
 import requests
-headers = {"Authorization": "Bearer ya29.a0Aa4xrXM8IaHeA6pslX0xr92LdRXY88DMXh6A912qhxD6bAt9-rkXeL8waxSQnCyM_z8ONa4LNxGHHACszrNwQH8fzDXVq2OtD6ELxL2f7y2gn6iAiWayQ_1tZkflLzRucdkjNjhR3NvcbnjN4s5b27kD_qBzaCgYKATASARASFQEjDvL9ljmKpWvHXa5fJS9h-T5_cQ0163"}
+headers = {"Authorization": "Bearer ya29.a0xrXM8IaHeA6pslX0xr92LdRXY88DMXh6A912qhxD6bAt9-rkXeL8waxSQnCyM_z8ONaxGHHACszrNwQH8fzDXVq2OtD6ELxL2f7y2gn6iAiWayQ_1tZkflLzRucdkjNjhR3NvcbnjN4s5b27kD_qBzaCgYKATASARASFQEjDvL9ljmKpWvHXa5fJS9h-T5_cQ0163"}
 para = {
     "name": "searching.jpg",
     "parents": ['1DpZEC-2cTjZIHh5wbzAr7AURLaAL8rEY'],
